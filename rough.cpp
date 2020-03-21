ff751591dd7954ab74c93c2635759abf67a93471
Prakash-sa <sainiprakash525@gmail.com>
2020-03-21 23:51:59 +0530

    new ones.

--- rough.cpp
+++ rough.cpp
@@ -1,114 +1,36 @@
-#include <bits/stdc++.h>
-
+#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
-
-string ltrim(const string &);
-string rtrim(const string &);
-vector<string> split(const string &);
-
-// Complete the freqQuery function below.
-vector<int> freqQuery(vector<vector<int>> queries) {
-    map<int,int>mp1,mp2;
-    vector<int>vv;
-    for(int i=0;i<queries.size();i++){
-        int k1=queries[i][0],k2=queries[i][1];
-        if(k1==1){
-            mp2[mp1[k2]]--;
-            mp1[k2]++;
-            mp2[mp1[k2]]++;
-        }
-        else if(k1==2){
-            mp2[mp1[k2]]--;
-            mp1[k2]--;
-            mp2[mp1[k2]]++;
-        }
-        else {
-            if(mp2[k2]>0)vv.push_back(1);
-            else vv.push_back(0);
+#define uint64_t int
+set<int>s;
+void ch(int n,int num){
+    queue<int>q;
+    q.push(num);
+    while(!q.empty()){
+        num=q.front();
+        q.pop();
+        if(num<=n){
+            s.insert(num);
+            int last=num%10;
+            if(last<9){
+                q.push(num*10+last+1);
+            }
+            if(last>0)q.push(num*10+last-1);
         }
     }
-return vv;
-
+    
 }
-
 int main()
-{
-    ofstream fout(getenv("OUTPUT_PATH"));
-
-    string q_temp;
-    getline(cin, q_temp);
-
-    int q = stoi(ltrim(rtrim(q_temp)));
-
-    vector<vector<int>> queries(q);
-
-    for (int i = 0; i < q; i++) {
-        queries[i].resize(2);
-
-        string queries_row_temp_temp;
-        getline(cin, queries_row_temp_temp);
-
-        vector<string> queries_row_temp = split(rtrim(queries_row_temp_temp));
-
-        for (int j = 0; j < 2; j++) {
-            int queries_row_item = stoi(queries_row_temp[j]);
-
-            queries[i][j] = queries_row_item;
-        }
+ {
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        for(int i=0;i<=9&&i<=n;i++)ch(n,i);
+        for(auto it=s.begin();it!=s.end();it++)cout<<*it<<" ";
+        cout<<endl;
+    
     }
-
-    vector<int> ans = freqQuery(queries);
-
-    for (int i = 0; i < ans.size(); i++) {
-        fout << ans[i];
-
-        if (i != ans.size() - 1) {
-            fout << "\n";
-        }
-    }
-
-    fout << "\n";
-
-    fout.close();
-
     return 0;
-}
-
-string ltrim(const string &str) {
-    string s(str);
-
-    s.erase(
-        s.begin(),
-        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
-    );
-
-    return s;
-}
-
-string rtrim(const string &str) {
-    string s(str);
-
-    s.erase(
-        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
-        s.end()
-    );
-
-    return s;
-}
-
-vector<string> split(const string &str) {
-    vector<string> tokens;
-
-    string::size_type start = 0;
-    string::size_type end = 0;
-
-    while ((end = str.find(" ", start)) != string::npos) {
-        tokens.push_back(str.substr(start, end - start));
-
-        start = end + 1;
-    }
-
-    tokens.push_back(str.substr(start));
-
-    return tokens;
 }
