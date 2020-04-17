792a2ed0414809192ad3c8aa103fa114186e019a
Prakash-sa <sainiprakash525@gmail.com>
2020-04-17 21:33:36 +0530

    17 Ap

--- algorithm/prime_factors.cpp
+++ algorithm/prime_factors.cpp
@@ -3,7 +3,21 @@ using namespace std;
 typedef  long long int lli;
 #define boost ios_base::sync_with_stdio(false); cin.tie(NULL);
 
-
+int getfactor(int x){
+  int cnt=0;
+  while (x%2==0) {
+    cnt++;
+    x=x/2;
+  }
+  for(int i=3;i<=sqrt(x);i+=2){
+    while(x%i==0){
+      cnt++;
+      x=x/i;
+    }
+  }
+  if(x>1)cnt++;
+  return cnt;
+}
 
 int main(){
   boost;
--- productmod.cpp
+++ productmod.cpp
@@ -21,3 +21,19 @@ int productExceptSelf(std::vector<int> nums, int m) {
     for(int i=0;i<ns;i++)g=(g+ans[i])%m;
     return g;
 }
+
+
+vector<int> productExceptSelf(vector<int>& nums) {
+        int n=nums.size();
+        int pre[n],suf[n];
+        pre[0]=nums[0],suf[n-1]=nums[n-1];
+        for(int i=1;i<n;i++)pre[i]=pre[i-1]*nums[i];
+        for(int i=n-2;i>=0;i--)suf[i]=suf[i+1]*nums[i];
+        vector<int>v;
+        v.push_back(suf[1]);
+        for(int i=1;i<n-1;i++){
+            v.push_back(pre[i-1]*suf[i+1]);
+        }
+        v.push_back(pre[n-2]);
+        return v;
+    }
