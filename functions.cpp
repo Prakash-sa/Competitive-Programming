a3d7d7e68dcba8ebb87ed7c93862f453906a3f83
Prakash-sa <sainiprakash525@gmail.com>
2020-04-18 15:19:23 +0530

    18 April Dp

--- functions.cpp
+++ functions.cpp
@@ -82,6 +82,20 @@ lli ncr(int n,int k){
     return c[n][k];
 }
 
+//lucas
+int nCrModpDP(int n, int r, int p) 
+{
+      int C[r+1];
+    memset(C, 0, sizeof(C));
+    C[0] = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = min(i, r); j > 0; j--)
+            C[j] = (C[j] + C[j-1])%p;
+    }
+    return C[r];
+}
+
 // fermats theorem
 ll f[1000005];
 ll power(ll a,ll b)
--- rough.cpp
+++ rough.cpp
@@ -3,34 +3,28 @@ using namespace std;
 typedef  long long int lli;
 #define boost ios_base::sync_with_stdio(false); cin.tie(NULL);
 
-//https://codeforces.com/problemset/problem/38/E
 
-pair<int,int>ar[3001];
-int n;
-lli dp[3001][3001];
-const long long int INF=1e15;
-
-int getAns(int pos,int pre){
-  if(pos>n)return 0;
-  if(dp[pos][pre]!=-INF)return dp[pos][pre];
-  //pin it
-  int x=ar[pos].second+getAns(pos+1,pos);
-
-  //leave it
-  int y=abs(ar[pos].first-ar[pre].first)+getAns(pos+1,pre);
-
-  return dp[pos][pre]=min(x,y);
+int nCrModpDP(int n, int r)
+{
+      int C[r+1];
+    memset(C, 0, sizeof(C));
+    C[0] = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = min(i, r); j > 0; j--)
+            C[j] = (C[j] + C[j-1]);
+    }
+    return C[r];
 }
 
 int main(){
   boost;
-  cin>>n;
-  for(int i=0;i<=n;i++){
-    for(int j=0;j<=n;j++)dp[i][j]=-INF;
+  int t,n1,n2;
+  cin>>t;
+  while(t--){
+    cin>>n1>>n2;
+    if(n1>=n2)
+    cout<<nCrModpDP(n1-n2,n2)<<endl;
+    else cout<<0<<endl;
   }
-  for(int i=1;i<=n;i++){
-    cin>>ar[i].first>>ar[i].second;
-  }
-  sort(ar+1,ar+n+1);
-  cout<<getAns(2,1)+ar[1].second<<endl;
 }
