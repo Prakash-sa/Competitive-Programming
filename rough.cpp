f521d17cc7c7bba30275b48c595adf35b12bdb74
Prakash-sa <sainiprakash525@gmail.com>
2020-06-14 11:43:29 +0530

    dp

--- rough.cpp
+++ rough.cpp
@@ -5,25 +5,29 @@ typedef  long long int lli;
 #define boost ios_base::sync_with_stdio(false); cin.tie(NULL);
 #define inf (1000*1000*1000+5)
 
-lli getR(lli a){
-  while(a%2==0)a=a/2;
-  return a;
-}
+
 
 int main(){
   boost;
   int t;
   cin>>t;
   while(t--){
-    lli n;
-    cin>>n;
-    lli ans=0;
-    for(int i=0;i<60;i++){
-      if(n&(1LL<<i))ans+=(1LL<<(i+1))-1;
+    string s;
+    cin>>s;
+    int zero=0,one=0;
+    for(int i=0;i<s.size();i++){
+      if(s[i]=='0')zero++;
+      else one++;
+    }
+    int ans=min(one,zero);
+    int pre0=0,pre1=0;
+    for(int i=0;i<s.size();i++){
+      pre0+=(s[i]=='0'),zero-=(s[i]=='0');
+      pre1+=(s[i]=='1'),one-=(s[i]=='1');
+      ans=min(ans,pre1+zero);
+      ans=min(ans,pre0+one);
     }
     cout<<ans<<endl;
-    
   }
-
-return 0;
+  return 0;
 }
