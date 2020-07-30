1566313ba1148d698657ff65fb68f806d5c03efa
Prakash-sa <sainiprakash525@gmail.com>
2020-07-30 12:25:12 +0530

    new ones

--- algorithm/cdechef.cpp
+++ algorithm/cdechef.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 #include<iostream>
 #include<bits/stdc++.h>
@@ -41,29 +40,4 @@ int main()
     }
 
 
-=======
-#include <bits/stdc++.h>
-using namespace std;
-typedef long long int lli;
-#define F(i,a,b) for(lli i = a; i <= b; i++)
-#define RF(i,a,b) for(lli i = a; i >= b; i--)
-#define endl "\n"
-#define speed ios_base::sync_with_stdio(false);cin.tie(NULL);
-
-int main()
-{
-    speed;
-    lli n,m;
-    cin>>n;
-    lli a[n];
-    for(lli i=0;i<n;i++)cin>>a[i];
-    sort(a,a+n);
-    for(lli i=1;i<n;i++){
-        a[i]=a[i]-a[i-1];
-       
-    }
-    if(a[n-1]==0)cout<<"YES"<<endl;
-    else cout<<"NO"<<endl;
-    return 0;
->>>>>>> 4e1dc46759781f5165e6668e36a23c0b03142970
 }
