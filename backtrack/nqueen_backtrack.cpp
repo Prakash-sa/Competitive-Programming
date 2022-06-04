3f6d54cd46b45f766c01d5091c5ec61d797d2bc3
Prakash-sa <sainiprakash525@gmail.com>
2022-06-04 11:39:24 +0530

    added backtrack

--- backtrack/nqueen_backtrack.cpp
+++ backtrack/nqueen_backtrack.cpp
@@ -76,3 +76,127 @@ int main()
 	}
 	return 0;
 }
+
+/*
+
+N-Queens 1
+*/
+
+//https://leetcode.com/problems/n-queens/
+
+
+class Solution {
+    vector<vector<string>>res;
+    
+    bool isValid(vector<string>&Q,int n,int row,int col){
+        //check if the column had a queen before.
+        for(int i=0;i<row;i++)if(Q[i][col]=='Q')return false;
+        //check if the 45° diagonal had a queen before.
+        for(int i=row-1,j=col-1;i>=0 && j>=0; i--,j--)if(Q[i][j]=='Q')return false;
+        //check if the 135° diagonal had a queen before.
+        for(int i=row-1,j=col+1;i>=0&& j<n;i--,j++)if(Q[i][j]=='Q')return false;
+        return true;
+    }
+    
+    void backtrack(vector<string>&Q,int x,int n){
+        
+        if(x==n){
+            res.push_back(Q);
+            return;
+        }
+        
+        for(int i=0;i<n;i++){
+            if(isValid(Q,n,x,i)){
+                Q[x][i]='Q';
+                backtrack(Q,x+1,n);
+                Q[x][i]='.';
+            }
+        }
+    }
+    
+public:
+    vector<vector<string>> solveNQueens(int n) {
+        vector<string>ans(n,string(n,'.'));
+        backtrack(ans,0,n);
+        return res;
+    }
+};
+
+
+
+
+
+/*
+
+N-Queens 2
+
+https://leetcode.com/problems/n-queens-ii/
+
+
+1. Approach:-
+Time Complexity:- O(N!)
+Space Complexity:- O(N*N);
+*/
+
+class Solution {
+    
+    int cnt;
+    
+    bool isValid(vector<vector<bool>>&ans,int row,int col,int n){
+        for(int i=0;i<row;i++)if(ans[i][col])return false;
+        for(int i=row-1,j=col+1;i>=0 && j<n;i--,j++)if(ans[i][j])return false;
+        for(int i=row-1,j=col-1;i>=0&&j>=0;i--,j--)if(ans[i][j])return false;
+        return true;
+    }
+    
+    int backtrack(vector<vector<bool>>&ans,int x,int n){
+        if(x==n)return ++cnt;
+        for(int i=0;i<n;i++){
+            if(isValid(ans,x,i,n)){
+                ans[x][i]=true;
+                cnt=backtrack(ans,x+1,n);
+                ans[x][i]=false;
+            }
+        }
+        return cnt;
+    }
+    
+public:
+    int totalNQueens(int n) {
+        cnt=0;
+        vector<vector<bool>>ans(n,vector<bool>(n,false));
+        return backtrack(ans,0,n);
+    }
+};
+
+/*
+2. Approach:-
+
+Time Complexity:- O(N!)
+Space Complexity:- O(N)
+*/
+
+
+class Solution {
+    
+    int cnt;
+    
+    int backtrack(vector<bool>&col,vector<bool>&diag,vector<bool>&anti_diag,int x,int n){
+        if(x==n)return ++cnt;
+        for(int i=0;i<n;i++){
+            if(!col[i] && !diag[x+i] && !anti_diag[x-i+n-1]){
+                col[i] = diag[x+i] = anti_diag[x-i+n-1]=true;
+                cnt=backtrack(col,diag,anti_diag,x+1,n);
+                col[i] = diag[x+i] = anti_diag[x-i+n-1]=false;
+            }
+        }
+        return cnt;
+    }
+    
+public:
+    int totalNQueens(int n) {
+        cnt=0;
+        vector<bool> col(n),diag(2*n-1),anti_diag(2*n-1);
+        return backtrack(col,diag,anti_diag,0,n);
+    }
+};
--- rough.cpp
+++ rough.cpp
@@ -31,28 +31,67 @@ int main()
 	while(t--){
 		int n;
 		cin>>n;
-		vector<int>a(n);
-		ans=0;
-		for(int i=0;i<n;i++){
-			cin>>a[i];
-		}
-		int flag=false;
-		if(a[n-1]>n || a[0]!=1){
-			cout<<-1<<endl;
-			// cout<<"aa";
+		int l=-1,r=-1;
+		if(__builtin_popcount(n)==1){
+			if(n==0)cout<<1;
+			else if(n==1)cout<<3;
+			else cout<<n+1;
+			cout<<endl;
+
 			continue;
 		}
-		for(int i=1;i<n;i++){
-			if(a[i]-a[i-1]>2)flag=true;
-		}
-		if(flag){
-			cout<<-1<<endl;
-		}
 		else {
-			cout<<n<<" ";
-			for(int i=1;i<n;i++)cout<<i<<" ";
-			cout<<endl;
+			for(int i=0;i<32;i++){
+				if(n&(1<<i)  && l<0)l=i;
+				else if((n&(1<<i))==0 && r<0){
+					// cout<<1;
+					r=i;
+				}
+				if(l>=0)break;
+			}
+			cout<<(1<<l)<<endl;
 		}
 	}
 	return 0;
 }
+
+
+// int main()
+// {
+// 	boost;
+// 	int t;
+// 	cin>>t;
+// 	while(t--){
+// 		int n;
+// 		cin>>n;
+// 		stack<lli>a;
+// 		bool cnt=false;
+// 		for(int i=0;i<n;i++){
+// 			lli k;cin>>k;
+// 			if(k%2==0)a.push(k);
+// 			else cnt=true;
+// 		}
+// 		int ans=0;
+// 		while(a.size()>1){
+// 			lli k=a.top();
+// 			a.pop();
+// 			k+=a.top();
+// 			a.pop();
+// 			ans++;
+// 			if(k%2==0){
+// 				ans++;
+// 				k=k/2;
+// 				if(k%2==0)
+// 				a.push(k);
+// 			}
+// 			else cnt=true;
+// 		}
+// 		if(a.size()==1 && cnt){
+// 			ans++;
+// 		}
+// 		cout<<ans<<endl;
+
+
+// 	}
+// 	return 0;
+// }
