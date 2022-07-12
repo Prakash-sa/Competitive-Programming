af8cc0286d7a296f21f43186e41a6d139c373791
Prakash-sa <sainiprakash525@gmail.com>
2022-07-12 17:11:14 +0530

    jump game

--- dp/jump_game.cpp
+++ dp/jump_game.cpp
@@ -1,5 +1,11 @@
 //https://leetcode.com/problems/jump-game/
+/*
+You are given an integer array nums. You are initially positioned at the array's first index, and each element in the array represents your maximum jump length at that position.
 
+Input: nums = [2,3,1,1,4]
+Output: true
+Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
+*/
 //Jump Game 1
 
 
@@ -21,7 +27,16 @@ public:
 
 
 //https://leetcode.com/problems/jump-game-ii/
+/*
 
+Given an array of non-negative integers nums, you are initially positioned at the first index of the array.
+Each element in the array represents your maximum jump length at that position.
+Your goal is to reach the last index in the minimum number of jumps.
+
+Input: nums = [2,3,1,1,4]
+Output: 2
+Explanation: The minimum number of jumps to reach the last index is 2. Jump 1 step from index 0 to 1, then 3 steps to the last index
+*/
 //Jump Game 2
 
 class Solution {
@@ -43,7 +58,21 @@ public:
 
 
 //https://leetcode.com/problems/jump-game-iii/
+/*
+Given an array of non-negative integers arr, 
+you are initially positioned at start index of 
+the array. When you are at index i,
+you can jump to i + arr[i] or i - arr[i], 
+check if you can reach to any index with value 0.
 
+
+Input: arr = [4,2,3,0,3,1,2], start = 5
+Output: true
+Explanation: 
+All possible ways to reach at index 3 with value 0 are: 
+index 5 -> index 4 -> index 1 -> index 3 
+index 5 -> index 6 -> index 4 -> index 1 -> index 3
+*/
 //Jump Game 3
 
 
@@ -75,7 +104,22 @@ public:
 
 
 //https://leetcode.com/problems/jump-game-iv/
+/*
+Given an array of integers arr, you are initially positioned at the first index of the array.
 
+In one step you can jump from index i to index:
+
+    i + 1 where: i + 1 < arr.length.
+    i - 1 where: i - 1 >= 0.
+    j where: arr[i] == arr[j] and i != j.
+
+Return the minimum number of steps to reach the last index of the array.
+
+Input: arr = [100,-23,-23,404,100,23,23,23,3,404]
+Output: 3
+Explanation: You need three jumps from index 0 --> 4 --> 3 --> 9. Note that index 9 is the last index of the array.
+
+*/
 //Jump Game 4
 
 
@@ -118,7 +162,22 @@ public:
 
 
 //https://leetcode.com/problems/jump-game-v/
+/*
+Given an array of integers arr and an integer d. In one step you can jump from index i to index:
 
+    i + x where: i + x < arr.length and 0 < x <= d.
+    i - x where: i - x >= 0 and 0 < x <= d.
+
+In addition, you can only jump from index i to index j if arr[i] > arr[j] and arr[i] > arr[k] for all indices k between i and j (More formally min(i, j) < k < max(i, j)).
+
+You can choose any index of the array and start jumping. Return the maximum number of indices you can visit.
+
+Input: arr = [6,4,14,6,8,13,9,7,10,6,12], d = 2
+Output: 4
+Explanation: You can start at index 10. You can jump 10 --> 8 --> 6 --> 7 as shown.
+Note that if you start at index 6 you can only jump to index 7. You cannot jump to index 5 because 13 > 9. You cannot jump to index 4 because index 5 is between index 4 and 6 and 13 > 9.
+Similarly You cannot jump from index 3 to index 2 or index 1.
+*/
 //Jump Game 5
 
 
@@ -190,7 +249,20 @@ public:
 
 
 //https://leetcode.com/problems/jump-game-vii/
+/*
+You are given a 0-indexed binary string s and two integers minJump and maxJump. In the beginning, you are standing at index 0, which is equal to '0'. You can move from index i to index j if the following conditions are fulfilled:
 
+    i + minJump <= j <= min(i + maxJump, s.length - 1), and
+    s[j] == '0'.
+
+Return true if you can reach index s.length - 1 in s, or false otherwise.
+
+Input: s = "011010", minJump = 2, maxJump = 3
+Output: true
+Explanation:
+In the first step, move from index 0 to index 3. 
+In the second step, move from index 3 to index 5.
+*/
 //Jump Game 7
 
 
