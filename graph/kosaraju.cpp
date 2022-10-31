810bdec4e10b1bb88939149324899934193623a7
Vinit Waingankar <87994131+vrwaingankar@users.noreply.github.com>
2022-10-30 19:39:04 -0500

    Remove comments

--- graph/kosaraju.cpp
+++ graph/kosaraju.cpp
@@ -31,7 +31,6 @@ void calc_time(int v,stack<int> &st,bool vis[],vector<int> adj[])
 void dfs(int v,bool vis[],vector<int> grev[])
 {
     vis[v]=true;
-    // cout<<v<<" ";
     for(auto i=grev[v].begin();i!=grev[v].end();i++)
     {
         if(vis[*i]==false)
@@ -40,7 +39,6 @@ void dfs(int v,bool vis[],vector<int> grev[])
 }
 int kosaraju(int V, vector<int> adj[])
 {
-    // print(adj,V);
     bool vis[V]={};
     stack<int> st;
     for(int v=0;v<V;v++)
@@ -48,7 +46,6 @@ int kosaraju(int V, vector<int> adj[])
         if(vis[v]==false)
             calc_time(v,st,vis,adj);
     }
-    // cout<<"stack_size="<<st.size()<<endl;
     //making new graph (grev) with reverse edges as in adj[]
     vector<int> grev[V];
     for(int i=0;i<V;i++)
@@ -59,8 +56,6 @@ int kosaraju(int V, vector<int> adj[])
             grev[*j].push_back(i);
         }
     }
-    // cout<<"grev="<<endl;
-    // print(grev,V);
     
     int cnt_scc=0;
     while(!st.empty())
@@ -91,7 +86,6 @@ int main()
             cin>>m>>n;
             adj[m].push_back(n);
         }
-        // exit(0);
         cout<<kosaraju(a, adj)<<endl;
     }
     return 0;
