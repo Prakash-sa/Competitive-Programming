5e9699bb7909aab50058d5e50edb48e8f0ff762b
Prakash-sa <sainiprakash525@gmail.com>
2023-01-27 23:59:42 -0600

    dp

--- 2 pointer/1658. Minimum Operations to Reduce X to Zero.cpp
+++ 2 pointer/1658. Minimum Operations to Reduce X to Zero.cpp
@@ -1,26 +1,26 @@
-//https://leetcode.com/problems/minimum-operations-to-reduce-x-to-zero/
+// https://leetcode.com/problems/minimum-operations-to-reduce-x-to-zero/
 
-//https://leetcode.com/problems/minimum-operations-to-reduce-x-to-zero/discuss/2136570/Change-Your-Perspective-or-JAVA-Explanation
+// https://leetcode.com/problems/minimum-operations-to-reduce-x-to-zero/discuss/2136570/Change-Your-Perspective-or-JAVA-Explanation
 
 /*
-You are given an integer array nums and an integer x. 
-In one operation, you can either remove the leftmost or the rightmost element from the array nums and subtract its value from x. 
+You are given an integer array nums and an integer x.
+In one operation, you can either remove the leftmost or the rightmost element from the array nums and subtract its value from x.
 Note that this modifies the array for future operations.
-Return the minimum number of operations to reduce x to exactly 0 if it is possible, 
+Return the minimum number of operations to reduce x to exactly 0 if it is possible,
 otherwise, return -1.
 */
 
-
-
-
-
-class Solution {
+class Solution
+{
 public:
-    int minOperations(vector<int>& nums, int x) {
+    int minOperations(vector<int> &nums, int x)
+    {
         int sum = accumulate(begin(nums), end(nums), 0);
         int l = 0, r = 0, res = INT_MAX, sz = nums.size();
-        while (l <= r){
-            if (sum >= x) {
+        while (l <= r)
+        {
+            if (sum >= x)
+            {
                 if (sum == x)
                     res = min(res, l + sz - r);
                 if (r < sz)
@@ -34,5 +34,3 @@ public:
         return res == INT_MAX ? -1 : res;
     }
 };
-
-
--- graph/(0-1 bfs)2290. Minimum Obstacle Removal to Reach Corner.cpp
+++ graph/(0-1 bfs)2290. Minimum Obstacle Removal to Reach Corner.cpp
@@ -3,9 +3,8 @@
 /*
 
 For every node that you visit, push all its unvisited neighbours to the queue in the following manner.
-
-    Every unvisited neighbour with cost 1 (having an obstacle), push to the end of the queue
-    Every unvisited neighbour with cost 0 (having no obstacle), push to the beginning of the queue.
+Every unvisited neighbour with cost 1 (having an obstacle), push to the end of the queue
+Every unvisited neighbour with cost 0 (having no obstacle), push to the beginning of the queue.
 
 */
 
@@ -18,7 +17,6 @@ public:
     int minimumObstacles(vector<vector<int>>& grid) {
         int n=grid.size(),m=grid[0].size();
         vector<vector<int>>dp(n,vector<int>(m,INT_MAX));
-        
         vector<vector<int>>vis(n,vector<int>(m,false));
         deque<pair<int,int>>q;
         q.push_front({0,0});
@@ -34,7 +32,6 @@ public:
                 int yy=y+dy[i];
                 if(xx<0||xx>=n || yy<0 || yy>=m || vis[xx][yy])continue;
                 // cout<<xx<<" "<<yy<<endl;
-                
                 if(grid[xx][yy]==0){
                     dp[xx][yy]=dp[x][y];
                     q.push_front({xx,yy});
