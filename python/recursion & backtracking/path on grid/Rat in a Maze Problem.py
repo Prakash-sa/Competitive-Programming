7d127bbb2a7f465c34d9bd99eae42d8bb2e5dcbb
Prakash-sa <sainiprakash525@gmail.com>
2025-09-22 12:34:13 -0600

    added

--- python/recursion & backtracking/path on grid/Rat in a Maze Problem.py
+++ python/recursion & backtracking/path on grid/Rat in a Maze Problem.py
@@ -28,3 +28,45 @@ Constraints:
 0 ≤ mat[i][j] ≤ 1
 '''
 
+class Solution:
+    # Function to find all possible paths
+    
+    def inside(self,r,c):
+        return 0<=r<self.n and 0<=c<self.n
+    
+    def dfs(self,r,c,path):
+        if r==self.n-1 and c==self.n-1:
+            self.res.append(path)
+            return
+        
+        self.visited[r][c]=True
+        
+        for ch,dr,dc in self.dirs:
+            nr,nc=r+dr,c+dc
+            if self.inside(nr,nc) and not self.visited[nr][nc] and self.maze[nr][nc]==1:
+                self.dfs(nr,nc,path+ch)
+                
+        self.visited[r][c]=False
+            
+            
+        
+    def ratInMaze(self, maze):
+        # code here
+        self.n=len(maze)
+        
+        if self.n==0 or maze[0][0]==0 or maze[self.n-1][self.n-1]==0:
+            return []
+        
+        self.maze=maze
+        self.res=[]
+        self.visited=[[False]*self.n for _ in range(self.n)]
+        
+        self.dirs=[('D',1,0),('L',0,-1),('R',0,1),('U',-1,0)]
+        
+        self.dfs(0,0,"")
+        self.res.sort()
+        return self.res
+        
+        
+# Time Complexity: O(4(m*n)), because on every cell we need to try 4 different directions.
+# Space Complexity:  O(m*n), Maximum Depth of the recursion tree(auxiliary space).
