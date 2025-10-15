12a378f4dad6bd00efd9f276dd820abf1a4033ec
Prakash-sa <sainiprakash525@gmail.com>
2025-10-15 13:00:25 -0600

    valid

--- python/2 Pointers/arrays/189. Rotate Array.py
+++ python/2 Pointers/arrays/189. Rotate Array.py
@@ -52,3 +52,45 @@ class Solution:
 # Complexity Analysis
 # Time complexity: O(n). n elements are reversed a total of three times.
 # Space complexity: O(1). No extra space is used.
+
+# https://www.geeksforgeeks.org/problems/rotate-array-by-n-elements-1587115621/1
+
+'''
+Given an array arr[]. Rotate the array to the left (counter-clockwise direction) by d steps, where d is a positive integer. Do the mentioned change in the array in place.
+
+Note: Consider the array as circular.
+
+Examples :
+
+Input: arr[] = [1, 2, 3, 4, 5], d = 2
+Output: [3, 4, 5, 1, 2]
+Explanation: when rotated by 2 elements, it becomes 3 4 5 1 2.
+Input: arr[] = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20], d = 3
+Output: [8, 10, 12, 14, 16, 18, 20, 2, 4, 6]
+Explanation: when rotated by 3 elements, it becomes 8 10 12 14 16 18 20 2 4 6.
+Input: arr[] = [7, 3, 9, 1], d = 9
+Output: [3, 9, 1, 7]
+Explanation: when we rotate 9 times, we'll get 3 9 1 7 as resultant array.
+Constraints:
+1 <= arr.size(), d <= 105
+0 <= arr[i] <= 105
+'''
+
+class Solution:
+    #Function to rotate an array by d elements in counter-clockwise direction.
+    
+    def rotateArr(self, arr, d):
+        #Your code here
+        n=len(arr)
+        d%=n
+        
+        def rev(arr,start,end):
+            while start<=end:
+                arr[start],arr[end]=arr[end],arr[start]
+                start+=1
+                end-=1
+        
+        rev(arr,0,d-1)
+        rev(arr,d,n-1)
+        rev(arr,0,n-1)
+        
---
+++ python/strings/125. Valid Palindrome.py
@@ -0,0 +1,53 @@
+# https://leetcode.com/problems/valid-palindrome/description/
+
+'''
+A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+Given a string s, return true if it is a palindrome, or false otherwise.
+
+ 
+
+Example 1:
+
+Input: s = "A man, a plan, a canal: Panama"
+Output: true
+Explanation: "amanaplanacanalpanama" is a palindrome.
+Example 2:
+
+Input: s = "race a car"
+Output: false
+Explanation: "raceacar" is not a palindrome.
+Example 3:
+
+Input: s = " "
+Output: true
+Explanation: s is an empty string "" after removing non-alphanumeric characters.
+Since an empty string reads the same forward and backward, it is a palindrome.
+ 
+
+Constraints:
+
+1 <= s.length <= 2 * 105
+s consists only of printable ASCII characters.
+'''
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        n=len(s)
+        l,r=0,n-1
+
+        while l<=r:
+            while l<r and not s[l].isalnum():
+                l+=1
+            while l<r and not s[r].isalnum():
+                r-=1
+            if s[l].lower()!=s[r].lower():
+                return False
+            l+=1
+            r-=1
+        return True
+
+
+# Complexity Analysis
+# Time complexity : O(n), in length n of the string. We traverse over each character at-most once, until the two pointers meet in the middle, or when we break and return early.
+# Space complexity : O(1). No extra space required, at all.
