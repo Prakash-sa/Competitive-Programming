e3553fc8724ca269abb527f891def3f1d7feb814
Prakash-sa <sainiprakash525@gmail.com>
2025-10-15 13:45:00 -0600

    lcs

--- python/strings/14. Longest Common Prefix.py
+++ python/strings/14. Longest Common Prefix.py
@@ -41,6 +41,16 @@ class Solution:
                     return ""
         return prefix
         
+'''
+Given a set of keys S = [S1 ,S2, …Sn], find the longest common prefix among a string q and S. This LCP query will be called frequently.
+
+We could optimize LCP queries by storing the set of keys S in a Trie. For more information about Trie, please see this article Implement a trie (Prefix trie). In a Trie, each node descending from the root represents a common prefix of some keys. But we need to find the longest common prefix of a string q and all key strings. This means that we have to find the deepest path from the root, which satisfies the following conditions:
+
+it is prefix of query string q
+each node along the path must contain only one child element. Otherwise the found path will not be a common prefix among all strings.
+the path doesn't comprise of nodes which are marked as end of key. Otherwise the path couldn't be a prefix a of key which is shorter than itself.
+'''
+
 ## Trie Solution
 
 class TrieNode:
@@ -89,3 +99,11 @@ class Solution:
         for s in strs[1:]:
             trie.insert(s)
         return trie.searchLongestPrefix(q)
+
+
+# Complexity Analysis
+# In the worst case query q has length m and it is equal to all n strings of the array.
+# Time complexity : preprocessing O(S), where S is the number of all characters in the array, LCP query O(m).
+# Trie build has O(S) time complexity. To find the common prefix of q in the Trie takes in the worst case O(m).
+
+# Space complexity : O(S). We only used additional S extra space for the Trie.
